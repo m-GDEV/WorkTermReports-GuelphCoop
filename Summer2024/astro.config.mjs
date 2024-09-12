47ace80ad6e238048e63d37bcc35dcce5d0eeed6
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-09-11 23:34:26 -0400

    work

--- Summer2024/astro.config.mjs
+++ Summer2024/astro.config.mjs
@@ -17,10 +17,27 @@ export default defineConfig({
                     label: "Summer 2024",
                     items: [
                         // Each item here is one entry in the navigation menu.
-                        { label: "Example Guide", slug: "guides/example" },
+                        // { label: "Example Guide", slug: "guides/example" },
                         {
                             label: "Introduction",
                             slug: "summer2024/introduction",
+                        },
+                        {
+                            label: "Information about the Employer",
+                            slug: "summer2024/informationabouttheemployer",
+                        },
+                        { label: "Goals", slug: "summer2024/goals" },
+                        {
+                            label: "Job Description",
+                            slug: "summer2024/jobdescription",
+                        },
+                        {
+                            label: "Conclusions",
+                            slug: "summer2024/conclusions",
+                        },
+                        {
+                            label: "Acknowledgements",
+                            slug: "summer2024/acknowledgements",
                         },
                     ],
                 },
--- Summer2024/src/content/docs/summer2024/introduction.md
+++
@@ -1,6 +0,0 @@
----
-title: Introduction
-description: Let's talk about my Summer 2024 Co-op position!
----
-
-### Let's talk about my Summer 2024 Co-op position!
